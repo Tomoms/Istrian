9db9b8a84cfa3b062e9f0e0f81809a189be59571
Tommaso Fonda <fondatommaso2@gmail.com>
2021-10-31 15:50:20 +0100

    command: NewCommand: implement the Command interface

--- src/main/java/org/istrian/command/NewCommand.java
+++ src/main/java/org/istrian/command/NewCommand.java
@@ -2,14 +2,29 @@ package org.istrian.command;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import org.istrian.Entry;
+import org.istrian.EntryType;
+import org.istrian.IstrianInstance;
+
+import java.util.NavigableSet;
 
 @Parameters(commandDescription = "Add a new entry to the database")
-public class NewCommand {
+public class NewCommand implements Command {
 
     @Parameter(names = { "-a", "--amount" })
     private double amount;
 
     @Parameter(names = { "-d", "--description" })
     private String description;
+
+    public void execute() {
+        final IstrianInstance instance = IstrianInstance.getInstance();
+        NavigableSet<Entry> set = instance.getSet();
+        int nextId = set.last().getId() + 1;
+        set.add(new Entry(nextId,
+                amount < 0 ? EntryType.OUTFLOW : EntryType.INCOME,
+                Math.abs(amount),
+                description));
+    }
 
 }
